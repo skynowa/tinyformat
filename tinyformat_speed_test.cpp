4244113c059694ef999eb9cba4d23a228bc5838a
skynowa <skynowa@gmail.com>
2014-12-01 21:39:16 +0200

    Review

--- tinyformat_speed_test.cpp
+++ tinyformat_speed_test.cpp
@@ -1,87 +1,116 @@
-#if defined(__linux__) && defined(__clang__)
-// Workaround for bug in gcc 4.4 standard library headers when compling with
-// clang in C++11 mode.
-namespace std { class type_info; }
-#endif
+/**
+ * \file   tinyformat_speed_test.cpp
+ * \brief
+ */
 
-#include <boost/format.hpp>
+
 #include <iomanip>
 #include <stdio.h>
 #include "tinyformat.h"
+#include <boost/format.hpp>
 #include <xLib/Core/Format.h>
-
-void speedTest(const std::string& which)
+//-------------------------------------------------------------------------------------------------
+void
+speedTest(
+    const std::string& a_testName
+)
 {
-    const long loops = 2000000L;
+    const std::size_t loops = 2000000;
 
-    #define str \
+    #define str1 \
         "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg" \
-        "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg" \
-        "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg" \
-        "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg" \
-        "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg" \
-        "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg" \
-        "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg" \
-        "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg" \
-        "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg" \
-        "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg"
+        "dsfgvsdfgshvdrtfghdfgh.,smdf.gknsd/f.sn;klfsn/dgmw4o5iut6c;w4o5ipt2[45pot[f" \
+        "2341c4io mweroic4posdfcgsdfgcsdfgdfgsdfgsdfgsdfgcsdytbyfghbjfjkfybkjfgxdcfg" \
+        "fghbjfghjfghjbfghjfghjbfghjfghjfgbhbjftgy jjk;.kpp,hgfvcddfghhnmknbdcfbgfcg" \
+        "asdcf'awsrk.fw3[045im2,[4oc'wxe;/fzd1[-,c4rxj.wk;szgfhbjgfh4-rx4rxqwetaertg" \
+        "acsfgvdbfghnjkllnffgcvgfbjnyhnl;/fzd1[-,c4rxj.wk;szlcgjx34-rx134rxqwetaertg" \
+        "asdcf'awsrk.fw3[045im2,[4oc'wdfgvdhfghbdfghdfgjbfghjbfgjhfghx134rxqwetaertg" \
+        "sdfcgsdfhgvdfghbfghbfgjbfgh1[-,c4rxj.wk;hfgszlcgjx3gfhjnhn4-rx134xqwetaertg" \
+        "asdcfsaert45vt[045im2,[4oc'sacfasdfae5bu6rb678u5687mcsdfacssacfasdfsafsdfcg" \
+        "ervtbe56un6i9ryvcae5tv6vb5n7m9i,098yob56ubr678mbfvtbyninbxhveujtybsrv56vye5"
 
     // Following is required so that we're not limited by per-character buffering
     std::ios_base::sync_with_stdio(false);
 
-    if (which == "std::printf") {
-        for (long i = 0; i < loops; ++ i) {
-            std::printf("%0.10f" str "%04d" str "%+g" str "%s" str "%p" str "%c" str "%%\n",
+    if (a_testName == "std::printf") {
+        char buff[1024] = {};
+
+        for (std::size_t i = 0; i < loops; ++ i) {
+            std::snprintf(buff, 1023, "%0.10f" str1 "%04d" str1 "%+g" str1 "%s" str1 "%p" str1 "%c" str1 "%%\n",
                 1.234, 42, 3.13, "string", (void *)1000, (int)'X');
         }
+
+        std::cout << buff << std::endl;
     }
-    else if (which == "std::iostreams") {
-        for (long i = 0; i < loops; ++ i) {
-            std::cout
+    else if (a_testName == "std::iostreams") {
+        std::string sRv;
+
+        for (std::size_t i = 0; i < loops; ++ i) {
+            std::stringstream ss;
+            ss
                 << std::setprecision(10) << std::fixed << 1.234
-                << std::resetiosflags(std::ios::floatfield) << str
-                << std::setw(4) << std::setfill('0') << 42 << std::setfill(' ') << str
-                << std::setiosflags(std::ios::showpos) << 3.13 << std::resetiosflags(std::ios::showpos) << str
-                << "string" << str
-                << (void *)1000 << str
-                << 'X' << str "%\n";
+                << std::resetiosflags(std::ios::floatfield) << str1
+                << std::setw(4) << std::setfill('0') << 42 << std::setfill(' ') << str1
+                << std::setiosflags(std::ios::showpos) << 3.13 << std::resetiosflags(std::ios::showpos) << str1
+                << "string" << str1
+                << (void *)1000 << str1
+                << 'X' << str1 "%\n";
+            sRv = ss.str();
         }
+
+        std::cout << sRv << std::endl;
     }
-    else if (which == "tinyformat") {
-        for (long i = 0; i < loops; ++ i) {
-            tfm::printf("%0.10f" str "%04d" str "%+g" str "%s" str "%p" str "%c" str "%%\n",
+    else if (a_testName == "tinyformat") {
+        std::string sRv;
+
+        for (std::size_t i = 0; i < loops; ++ i) {
+            sRv = tfm::format("%0.10f" str1 "%04d" str1 "%+g" str1 "%s" str1 "%p" str1 "%c" str1 "%%\n",
                 1.234, 42, 3.13, "string", (void *)1000, (int)'X');
         }
+
+        std::cout << sRv << std::endl;
     }
-    else if (which == "boost::format") {
-        for (long i = 0; i < loops; ++ i) {
-            std::cout << boost::format("%0.10f" str "%04d" str "%+g" str "%s" str "%p" str "%c" str "%%\n")
-                % 1.234 % 42 % 3.13 % "string" % (void *)1000 % (int)'X';
+    else if (a_testName == "boost::format") {
+        std::string sRv;
+
+        for (std::size_t i = 0; i < loops; ++ i) {
+            sRv = (boost::format("%0.10f" str1 "%04d" str1 "%+g" str1 "%s" str1 "%p" str1 "%c" str1 "%%\n")
+                % 1.234 % 42 % 3.13 % "string" % (void *)1000 % (int)'X').str();
         }
+
+        std::cout << sRv << std::endl;
     }
-    else if (which == "xlib::core::String::format") {
-        for (long i = 0; i < loops; ++ i) {
-            std::cout << xlib::core::String::format("%0.10f" str "%04d" str "%+g" str "%s" str "%p" str "%c" str "%%\n",
+    else if (a_testName == "xlib::core::String::format") {
+        std::string sRv;
+
+        for (std::size_t i = 0; i < loops; ++ i) {
+            sRv = xlib::core::String::format("%0.10f" str1 "%04d" str1 "%+g" str1 "%s" str1 "%p" str1 "%c" str1 "%%\n",
                 1.234, 42, 3.13, "string", (void *)1000, (int)'X');
         }
+
+        std::cout << sRv << std::endl;
     }
-    else if (which == "xlib::Format") {
-        for (long i = 0; i < loops; ++ i) {
-            std::cout << xlib::core::Format("{}" str "{}" str "{}" str "{}" str "{}" str "{}" str "{}\n",
+    else if (a_testName == "xlib::Format") {
+        std::string sRv;
+
+        for (std::size_t i = 0; i < loops; ++ i) {
+            sRv = xlib::core::Format("{}" str1 "{}" str1 "{}" str1 "{}" str1 "{}" str1 "{}" str1 "{}\n",
                 1.234, 42, 3.13, "string", (void *)1000, (int)'X');
         }
+
+        std::cout << sRv << std::endl;
     }
     else {
-        assert(0 && "Speed test for which version?");
+        std::cout << "Speed test for a_testName version?????" << std::endl;
     }
 }
-
-
+//-------------------------------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
     if (argc >= 2) {
-        speedTest(argv[1]);
+        ::speedTest(argv[1]);
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
+//-------------------------------------------------------------------------------------------------
